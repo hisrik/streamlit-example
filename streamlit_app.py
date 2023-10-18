5fbf36c83f4f711ad4dcb71330b744bfb09a9fe6
hisrik <148368549+hisrik@users.noreply.github.com>
2023-10-18 12:09:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import math
 import pandas as pd
 import streamlit as st
 
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
 st.title("Ethoca app")
 st.text_input('input your prompt here')
 # st.write(response)
