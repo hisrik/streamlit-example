d1d8b4d7acdc2a5fecce111bee09ffe01fee5d09
hisrik <148368549+hisrik@users.noreply.github.com>
2023-10-18 12:03:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import math
 import pandas as pd
 import streamlit as st
 
-st.title('Ethoca app")
-# st.text_input('input your prompt here')
+st.title("Ethoca app")
+st.text_input('input your prompt here')
 # st.write(response)
